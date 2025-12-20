9a8433ed66f052f6e5fa75687469b981a6c6f954
smiguelorlando898-dotcom <smiguelorlando898@gmail.com>
2025-12-20 07:03:56 -0500

    Actualizar config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Token del bot (OBLIGATORIO - obtener de @BotFather)
-TOKEN = os.getenv("TOKEN", "TU_TOKEN_AQUÍ")
+TOKEN = os.getenv("TOKEN", "8530361444:AAFZ-yZIFzDC0CVUvX-W14kTZGVKFITGBCE")
 
 # Tu user_id como administrador (OBLIGATORIO)
 # Para obtener tu user_id: envía /start a @userinfobot
-ADMIN_ID = int(os.getenv("ADMIN_ID", 123456789))  # Reemplaza con tu ID real
+ADMIN_ID = int(os.getenv("ADMIN_ID", @landitho9))  # Reemplaza con tu ID real
 
 # Configuración de descargas
 DOWNLOAD_PATH = "downloads"
