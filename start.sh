15c078c3b54c0bae1855061c7102353ffab26aea
smiguelorlando898-dotcom <smiguelorlando898@gmail.com>
2025-12-25 17:28:28 -0500

    Actualizar start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Iniciar servidor gráfico virtual
+
+# Inicia servidor gráfico virtual
 Xvfb :0 -screen 0 1024x768x16 &
-export DISPLAY=:0
 
-# Iniciar gestor de ventanas ligero
-fluxbox &
+# Inicia VNC con contraseña
+x11vnc -display :0 -rfbport 5900 -usepw -forever &
 
-# Iniciar servidor VNC
-x11vnc -display :0 -nopw -forever -shared &
+# Inicia escritorio XFCE
+startxfce4 &
 
-# Iniciar noVNC en puerto 8080 (corregido)
-# En versiones recientes de noVNC se usa 'novnc_proxy' en lugar de 'launch.sh'
-/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 8080
+# Inicia noVNC en puerto 8080
+/opt/noVNC/utils/launch.sh --vnc localhost:5900 --listen 8080
